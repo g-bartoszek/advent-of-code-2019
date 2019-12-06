5cafe49acc0e56bdc627f3f73fc5cdb2cf61adcf
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-12-06 11:47:31 +0100

    refactor

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::io::BufRead;
 
 type Data = std::collections::HashMap<String, std::collections::HashSet<String>>;
@@ -27,21 +26,11 @@ fn find_route(data: &Data, from: Option<&String>, current: &String, current_leng
 
 }
 
-fn main() {
+fn get_or_insert<K: std::cmp::Eq + std::hash::Hash, T:Default>(map: &mut std::collections::HashMap<K, T>, key: K) -> &mut T {
+    map.entry(key).or_insert(T::default())
+}
 
- /*   let input = "COM)B
-    B)C
-    C)D
-    D)E
-    E)F
-    B)G
-    G)H
-    D)I
-    E)J
-    J)K
-    K)L
-    K)YOU
-    I)SAN";*/
+fn main() {
 
     let file = std::fs::File::open("input").unwrap();
     let input = std::io::BufReader::new(file);
@@ -51,17 +40,8 @@ fn main() {
     for l in input.lines() {
         let line = l.unwrap();
         let o = line.trim().split(')').collect::<Vec<_>>();
-        match data.get_mut(o[0]) {
-            Some(entry) => {entry.insert(o[1].to_string()); },
-            None => {
-                let mut set = HashSet::new();
-                set.insert(o[1].to_string());
-                data.insert(o[0].to_string(), set);
-            },
-        }
+        get_or_insert(&mut data, o[0].to_string()).insert(o[1].to_string());
     }
-
-    //println!("Data {:?}", data);
 
 
     println!("Result: {}", data.keys().map(|k| count(&data, k)).sum::<usize>());
@@ -70,18 +50,9 @@ fn main() {
 
     for (ref k, ref v) in data {
         for n in v {
-            match neighbors.get_mut(n) {
-                Some(entry) => {entry.insert(k.clone());},
-                None => {
-                    let mut set = HashSet::new();
-                    set.insert(k.clone());
-                    neighbors.insert(n.clone(), set);
-                },
-            }
+            get_or_insert(&mut neighbors, n.clone()).insert(k.clone());
         }
     }
-
-    //println!("Neighbors {:?}", neighbors);
 
     find_route(&neighbors, None, &String::from("YOU"), 0);
 }
