ea10eed4319fd17c56049cc938f22aa01ecf6b6f
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-12-12 11:58:42 +0100

    day12 not enough memory

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,12 +1,48 @@
 use regex::Regex;
 use itertools::Itertools;
+use std::cmp::Ordering;
+use std::hash::{Hash, Hasher};
 
 type Vector = (i32, i32, i32);
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Default)]
 struct Moon {
     position: Vector,
     velocity: Vector,
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+struct Snapshot {
+    energy: i32,
+    moons: [Moon; 4],
+}
+
+impl Hash for Snapshot {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.energy.hash(state)
+    }
+}
+
+impl Snapshot {
+    fn new(input: &Vec<Moon>) -> Self {
+        let mut moons=  [Moon::default(); 4];
+
+        moons.copy_from_slice(&input);
+
+        Snapshot{energy: moons.iter().map(Moon::energy).sum::<i32>(), moons}
+    }
+}
+
+impl Ord for Snapshot {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.energy != other.energy {
+            self.energy.cmp(&other.energy)
+        }  else {
+            self.moons.cmp(&other.moons)
+        }
+    }
+
 }
 
 impl Moon {
@@ -57,14 +93,21 @@ fn main() {
     <x=3, y=5, z=-1>\n\
     ";
 
-    let input = "\
+    let input3 = "\
+    <x=-8, y=-10, z=0>\n\
+    <x=5, y=5, z=10>\n\
+    <x=2, y=-7, z=3>\n\
+    <x=9, y=-8, z=-3>\n\
+    ";
+
+    let input2 = "\
     <x=-8, y=-18, z=6>\n\
     <x=-11, y=-14, z=4>\n\
     <x=8, y=-3, z=-10>\n\
     <x=-2, y=-16, z=1>\n\
     ";
 
-    let mut moons = input.lines().map(|l| {
+    let mut moons = input2.lines().map(|l| {
         let captures = Regex::new(r"<x=(.*), y=(.*), z=(.*)>").unwrap().captures(l).unwrap();
         println!("{:?}", captures);
         Moon{
@@ -76,7 +119,33 @@ fn main() {
 
     println!("{:?}", moons);
 
-    for s in 1..1001 {
+    let mut step = 0usize;
+    let mut snapshots = std::collections::HashSet::<Snapshot>::new();
+    //let mut snapshots = Vec::<Snapshot>::new();
+    let mut now = std::time::SystemTime::now();
+    loop {
+        let snapshot = Snapshot::new(&moons);
+        if !snapshots.insert(snapshot) {
+            println!("!!! {}", step);
+            break;
+        }
+        //if snapshots.contains(&snapshot) {
+        //    println!("!!! {}", step);
+        //    break;
+        //}
+        //snapshots.push(snapshot);
+
+        if step % 1000000 == 0 {
+            println!("Step: {} Elapsed: {:?} Memory: {} MB, Size of a single snapshot: {} B",
+                     step,
+                     now.elapsed().unwrap().as_secs(),
+                     snapshots.len() * std::mem::size_of::<Snapshot>() / 1000000,
+                     std::mem::size_of::<Snapshot>(),
+            );
+            //now = std::time::SystemTime::now();
+        }
+
+        step += 1;
 
       for m in 0..moons.len() {
           for o in 0..moons.len() {
@@ -93,8 +162,6 @@ fn main() {
           m.advance();
       }
 
-      println!("{} {:?} {}",s,  moons, moons.iter().map(Moon::energy).sum::<i32>());
     }
-
 
 }
