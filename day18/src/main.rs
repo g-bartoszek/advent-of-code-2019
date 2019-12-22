e07841dd81f8aaf7a3c71af9fcf3f578bbb5b1ea
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-12-22 19:44:18 +0100

    day 18 still wip

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::BufRead;
-use std::cmp::Ordering;
+use std::cmp::{Ordering, Reverse};
+use std::thread::current;
 
 #[derive(PartialEq, Debug, Clone)]
 enum Field {
@@ -14,70 +15,13 @@ enum Field {
 type Point = (i32,i32);
 type Map = std::collections::HashMap<Point, Field>;
 
-fn main() {
-    let input = std::io::BufReader::new(std::fs::File::open("input").unwrap());
-    let mut map = Map::new();
-    let mut keys = Vec::<(char, Point)>::new();
-    let mut doors = std::collections::HashMap::<char, Point>::new();
-    let mut start = (0,0);
-
-    for (y, l) in input.lines().enumerate() {
-        for (x, c) in l.unwrap().chars().enumerate() {
-            print!("{}", c);
-            let x = x as i32;
-            let y = y as i32;
-            map.insert((x,y), match c {
-                '#' => Field::Wall,
-                '.' => Field::Empty,
-                '@' => {
-                    start = (x,y);
-                    Field::Empty
-                },
-                f if f.is_uppercase() => {
-                    doors.insert(f, (x,y));
-                    (Field::Door(f))
-                },
-                f if f.is_lowercase() => {
-                    keys.push((f, (x,y)));
-                    (Field::Key(f))
-                },
-                _ => panic!("Unrecognized field {}", c)
-            });
-        }
-        println!();
-    }
-
-    println!("Doors: {} Keys: {}", doors.len(), keys.len());
-
-    for (k,p) in &keys {
-        if let Some(_) = find_path(&map, start, *p, &keys) {
-            println!("From start to: {:?}", k);
-        } else {
-            for (d, dp) in &doors {
-                if let Some(_) = find_path(&map, *dp, *p, &keys) {
-                    println!("From {} to: {}", k, d);
-                }
-            }
-        }
-    }
-
-    let mut min = std::usize::MAX;
-    search(&map, start, keys.clone(), &doors);
-
-}
-
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
-struct State {
-    remaining_keys: Vec<(char, Point)>,
-    distance: usize,
-    position: Point,
-    went_to: Vec<char>
-}
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+struct State(Point,usize,std::collections::BTreeSet<char>);
 
 impl State {
-    fn weight(&self) -> usize {
-        self.distance // + self.remaining_keys.len() * 100
-    }
+   fn weight(&self) -> usize {
+       self.1
+   }
 }
 
 impl Ord for State {
@@ -92,49 +36,110 @@ impl PartialOrd for State {
     }
 }
 
-fn dist(l: Point, r: Point) -> usize {
-    ((l.0 - r.0).abs() + (l.1 - r.1).abs()) as usize
-}
+fn main() {
+    let input = std::io::BufReader::new(std::fs::File::open("input").unwrap());
+    let mut map = Map::new();
+    let mut places = Vec::<Point>::new();
+    let mut start = (0,0);
 
-fn search(map: &Map,
-          start: Point,
-          remaining_keys: Vec<(char, Point)>,
-          doors: &std::collections::HashMap<char, Point>) {
-
-    let mut open = std::collections::BinaryHeap::<std::cmp::Reverse<State>>::new();
-
-    open.push(std::cmp::Reverse(State{remaining_keys, distance: 0, position: start, went_to: vec![]}));
-
-    loop {
-        //println!("{:?}", open.len());
-        let current = open.pop().unwrap().0;
-
-        //println!("REMAINING: {}: DISTANCE: {}", current.remaining_keys.len(), current.distance);
-        //println!("{} {:?}", current.weight(), current);
-
-
-        if current.remaining_keys.is_empty() {
-            println!("FOUND: {}", current.distance);
-            return;
+    for (y, l) in input.lines().enumerate() {
+        for (x, c) in l.unwrap().chars().enumerate() {
+            print!("{}", c);
+            let x = x as i32;
+            let y = y as i32;
+            map.insert((x,y), match c {
+                '#' => Field::Wall,
+                '.' => Field::Empty,
+                '@' => {
+                    places.push((x,y));
+                    start = (x,y);
+                    Field::Empty
+                },
+                f if f.is_uppercase() => {
+                    places.push((x,y));
+                    (Field::Door(f))
+                },
+                f if f.is_lowercase() => {
+                    places.push((x,y));
+                    (Field::Key(f))
+                },
+                _ => panic!("Unrecognized field {}", c)
+            });
         }
+        println!();
+    }
 
-        for (key, position) in &current.remaining_keys {
-            //println!("Path to: {}", key);
-            if let Some(distance) = find_path(map, current.position, *position, &current.remaining_keys){
-                //println!("Found path path to: {}", key);
-                //println!("Distance: {}", now.elapsed().unwrap().as_micros());
-                let new_keys = current.remaining_keys.iter().filter(|(k, _)| *k != *key).map(|k| *k).collect::<Vec<_>>();
-                let mut went_to = current.went_to.clone();
-                went_to.push(*key);
-                let new_state = State{remaining_keys: new_keys, distance: current.distance + distance, position: *position, went_to};
-                open.push(std::cmp::Reverse(new_state));
+    let number_of_keys = map.iter().filter(|(_, f)| if let Field::Key(_) = f {
+        true
+    } else {
+        false
+    }).count();
+
+    println!("Keys to find: {}", number_of_keys);
+
+    let mut paths = std::collections::HashMap::<Point, Vec<(Point, usize)>>::new();
+
+    for from in &places {
+        for to in &places {
+            if from == to {
+                continue;
+            }
+
+            if let Some(d) = find_path(&map, *from, *to) {
+                paths.entry(*from).or_insert(Vec::<(Point, usize)>::new()).push((*to, d));
             }
         }
     }
 
+    for p in &paths {
+        println!("{:?}", p);
+    }
+    let mut open = std::collections::BinaryHeap::<Reverse<State>>::new();
+
+    open.push(Reverse(State(start, 0, std::collections::BTreeSet::<char>::new())));
+
+    loop {
+        //println!("{:?}", open);
+        //std::io::stdin().read_line(&mut String::new());
+
+        let current = open.pop().unwrap().0;
+
+        //println!("Current {:?}", current);
+        if current.2.len() == number_of_keys {
+            println!("SUCCESS: {} ", current.1);
+            return;
+        }
+
+        let current_pos = current.0;
+
+        for path in &paths[&current.0] {
+            let mut new_keys = current.2.clone();
+            match map[&path.0] {
+                Field::Door(d) => {
+                    if !current.2.contains(&d.to_lowercase().next().unwrap()) {
+                        //println!("Skipping path because of closed doors {:?}", path);
+                        continue;
+                    }
+                },
+                Field::Key(k) => {
+                    new_keys.insert(k);
+                }
+                _ => {}
+            }
+
+            //let mut visited = current.3.clone();
+            //visited.push(path.0);
+
+            //println!("Added path {:?}", path);
+            let new_state = Reverse(State(path.0, current.1 + path.1, new_keys));
+            if open.iter().find(|&e| *e == new_state).is_none() {
+                open.push(new_state);
+            }
+        }
+    }
 }
 
-fn find_path(map: &Map, start: Point, target: Point, remaining_keys: &Vec<(char, Point)>) -> Option<usize> {
+fn find_path(map: &Map, start: Point, target: Point) -> Option<usize> {
     let mut examined = std::collections::HashSet::<Point>::new();
     let mut open = std::collections::HashSet::<(Point,usize)>::new();
 
@@ -169,18 +174,11 @@ fn find_path(map: &Map, start: Point, target: Point, remaining_keys: &Vec<(char,
             //println!("Neighbor: {:?} {:?}", neighbor, map.get(&neighbor).unwrap_or(&Field::Undefined));
             if *neighbor == target {
                 return Some(distance);
-            } else if !examined.contains(neighbor) && {
+            } else if !examined.contains(neighbor) {
                 let field = map.get(&neighbor).unwrap_or(&Field::Undefined);
-                if let Field::Key(key) = field {
-                    remaining_keys.iter().find(|(k, _)| *k == *key).is_none()
-                } else if let Field::Door(d) = field {
-                    remaining_keys.iter().find(|(k, _)| *k == d.to_lowercase().next().unwrap()).is_none()
-                } else {
-                    *field == Field::Empty
+                if *field == Field::Empty {
+                    open.insert((*neighbor, distance));
                 }
-            } {
-                //println!("Added");
-                open.insert((*neighbor, distance));
             }
         }
     }
